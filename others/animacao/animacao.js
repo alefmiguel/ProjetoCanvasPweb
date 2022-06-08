42e13350b168c023617488f4edb414ed919465fb
ÁLEF MIGUEL DE SANTANA <2020313768@IFAL.LOCAL>
2022-06-08 14:20:28 -0300

    fazendo exemplo completo do cap20 casa do codigo

--- others/animacao/animacao.js
+++ others/animacao/animacao.js
@@ -1,30 +1,7 @@
-var canvas = document.getElementById('canvas-animacao');
-var ctx = canvas.getContext('2d');
-
-var b1 = new Bola(ctx);
-b1.x = 100;
-b1.y = 200;
-b1.velocidadeX = 20;
-b1.velocidadeY = -10;
-b1.cor = 'blue';
-b1.raio = 20;
-
-var b2 = new Bola(ctx);
-b2.x = 200;
-b2.y = 100;
-b2.velocidadeX = -10;
-b2.velocidadeY = 20;
-b2.cor = 'black';
-b2.raio = 30;
-
-var animacao = new Animacao();
-animacao.novoSprite(b1);
-animacao.novoSprite(b2);
-animacao.ligar();
-
-function Animacao() {
+function Animacao(context) {
 	this.sprites = [];
 	this.ligado = false;
+	this.context = context;
 }
 
 Animacao.prototype = {
@@ -41,8 +18,13 @@ Animacao.prototype = {
 		this.ligado = false;
 	},
 
+	limparTela: function() {
+		var ctx = this.context;
+		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	},
+
 	proximoFrame: function() {
-		if (! this.ligado) return;
+		if (! this.ligado) return; // verifica
 
 		this.limparTela();
 
@@ -55,6 +37,6 @@ Animacao.prototype = {
 		var animacao = this;
 		requestAnimationFrame(function() {
 			animacao.proximoFrame();
-		});
+		})
 	}
 }
--- script/calc.js
+++ script/calc.js
@@ -1,35 +1,35 @@
-//* CONTEXTO GRÁFICO COM CANVAS */
+// //* CONTEXTO GRÁFICO COM CANVAS */
 
-//! USANDO PARA ANIMAÇÕES NO CARREGAMENTO DA PAGE */
-window.onload = function() {
-	// aqui trabalharemos com o canvas
-}
+// //! USANDO PARA ANIMAÇÕES NO CARREGAMENTO DA PAGE */
+// window.onload = function() {
+// 	// aqui trabalharemos com o canvas
+// }
 
-//! USO DO GETCONTEXT DO CANVAS */
-//? PRIMEIRO EU CAPTURO
-//? EM SEGUIDA, DETERMINO O CONTEXTO
-// REFERENCIANDO O CANVAS
-var canvas = document.getElementById('canvas-name');
-//? document.getQuerySelector('#canvas-name') */
+// //! USO DO GETCONTEXT DO CANVAS */
+// //? PRIMEIRO EU CAPTURO
+// //? EM SEGUIDA, DETERMINO O CONTEXTO
+// // REFERENCIANDO O CANVAS
+// var canvas = document.getElementById('canvas-name');
+// //? document.getQuerySelector('#canvas-name') */
 
-// REFERENCIANDO O CONTEXTO DO CANVAS
-var context = canvas.getContext("2d");
+// // REFERENCIANDO O CONTEXTO DO CANVAS
+// var context = canvas.getContext("2d");
 
-//! SISTEMA DE COORDENADAS */
-// é como se fosse um plano cartesiano
-// x = horizontal / y = vertical
+// //! SISTEMA DE COORDENADAS */
+// // é como se fosse um plano cartesiano
+// // x = horizontal / y = vertical
 
-//! MÉTODO FILLRECT e STROKERECT */
-// propriedades para o desenho de retângulos etc
-fillRect(x, y, largura, altura) //? fundo
-strokeRect(x, y, largura, altura) //? contorno
+// //! MÉTODO FILLRECT e STROKERECT */
+// // propriedades para o desenho de retângulos etc
+// fillRect(x, y, largura, altura) //? fundo
+// strokeRect(x, y, largura, altura) //? contorno
 
-// PRIMEIRO COMEÇA DO PONTO SUPERIOR ESQUERDO
-// DEPOIS, O RETÂNGULO VAI P DIREITA E P BAIXO
+// // PRIMEIRO COMEÇA DO PONTO SUPERIOR ESQUERDO
+// // DEPOIS, O RETÂNGULO VAI P DIREITA E P BAIXO
 
-//! EXEMPLO PRÁTICO DA COLORIZAÇÃO */
-context.fillRect(50, 50, 100, 100);
-// posição x, posição y, largura, altura
+// //! EXEMPLO PRÁTICO DA COLORIZAÇÃO */
+// context.fillRect(50, 50, 100, 100);
+// // posição x, posição y, largura, altura
 
 //! CAMINHOS ou PATHS */
 // desenhos mais complexos
