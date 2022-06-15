0b6ff643fc180e38cce6afdbabb6401453cf6a50
ÁLEF MIGUEL DE SANTANA <2020313768@IFAL.LOCAL>
2022-06-15 13:34:20 -0300

    DE FATO FINALIZANDO O CAP 04

--- others/clipping/sonic.js
+++ others/clipping/sonic.js
@@ -3,11 +3,7 @@ var SONIC_ESQUERDA = 2;
 
 function Sonic(ctx, teclado, imagem) {
     // CONSTRUTOR
-<<<<<<< HEAD
     this.velocidade = 8;
-=======
-    this.velocidade = 10;
->>>>>>> 3f5033db84513ef918836993b346c6657ce97454
     this.ctx = ctx;
     this.teclado = teclado;
     this.x = 0;
@@ -29,17 +25,11 @@ Sonic.prototype = {
                 this.sheet.coluna = 0;
             }
 
-<<<<<<< HEAD
             this.andando = true;
             this.direcao = SONIC_DIREITA;
             this.sheet.proximoQuadro();
             this.x += this.velocidade;
     
-=======
-            this.sheet.proximoQuadro();
-            this.direcao = SONIC_DIREITA;
-            this.x += this.velocidade;
->>>>>>> 3f5033db84513ef918836993b346c6657ce97454
         }
 
         else if (this.teclado.pressionada(SETA_ESQUERDA)) {
@@ -48,17 +38,11 @@ Sonic.prototype = {
                 this.sheet.coluna = 0;
             }
 
-<<<<<<< HEAD
             this.andando = true;
             this.direcao = SONIC_ESQUERDA;
             this.sheet.proximoQuadro();
             this.x -= this.velocidade;
             
-=======
-            this.sheet.proximoQuadro();
-            this.x -= this.velocidade;
-            this.direcao = SONIC_ESQUERDA;
->>>>>>> 3f5033db84513ef918836993b346c6657ce97454
         }
 
         else {
